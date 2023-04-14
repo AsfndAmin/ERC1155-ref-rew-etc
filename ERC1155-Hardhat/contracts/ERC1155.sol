952b671c92c9568b4c4256cd18c5e5c9d0a44c29
Asfand Amin <99183246+AsfndAmin@users.noreply.github.com>
2023-04-15 02:49:41 +0500

    Update ERC1155.sol

--- ERC1155-Hardhat/contracts/ERC1155.sol
+++ ERC1155-Hardhat/contracts/ERC1155.sol
@@ -1,47 +1,57 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/extensions/ERC1155URIStorage.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
+import "@openzeppelin/contracts/utils/Strings.sol"; 
 
-contract MyERC1155 is ERC1155, Ownable, ReentrancyGuard {
+contract MyERC1155 is ERC1155URIStorage , Ownable, ReentrancyGuard {
 
     using Strings for uint256;
 
-    uint256 private constant TIER_1 = 1;
-    uint256 private constant TIER_2 = 2;
-    uint256 private constant TIER_3 = 3;
-    uint256 private constant TIER_4 = 4;
+    uint8 private constant TIER_1 = 1;
+    uint8 private constant TIER_2 = 2;
+    uint8 private constant TIER_3 = 3;
+    uint8 private constant TIER_4 = 4; 
 
-    mapping(uint256 => uint256) private tokenCounts;
+    mapping(uint256 => uint256) public tokenCounts;
     mapping(uint256 => uint256) private mintPrices;
-    mapping(uint256 => string) private tokenURIs;
 
-    // Mapping to keep track of the NFT IDs of all types and their owners
-    mapping(address => mapping(uint256 => uint256[])) private _nftIds;
 
     uint256 private raisedCap;
     uint256 private totalSaleCap;
+    uint256[] public store;
 
-    constructor(string memory _uri, uint256 _saleCap) ERC1155(_uri) {
+    
+
+    constructor(string memory _uri, uint256 _saleCap) ERC1155(_uri) { 
         totalSaleCap = _saleCap;
     }
 
     function mint(uint256 tier, address account) public payable nonReentrant {
         require(tier >= 1 && tier <= 4, "Invalid tier");
-        require(msg.value == mintPrices[tier], "Insufficient payment");
+        require(msg.value == mintPrices[tier], "Insufficient/wrong payment");
         require(msg.value + raisedCap <= totalSaleCap, "cannot mint more");
         uint256 tokenId;
 
             tokenCounts[tier]++;
             tokenId = tokenCounts[tier];
+            uint256 id  = tier * 10**uint256(digit(tokenId)) + tokenId;
+            store.push(id);
             
-        uint256[] storage ids = _nftIds[account][tier];
-        ids.push(tokenId);
-        _mint(account, tier, 1, "");
+        _mint(account, id, 1, "");
     }
+
+    function digit(uint256 n) internal pure returns (uint256) {
+        uint256 digits = 0;
+         while (n != 0) {
+         n /= 10;
+         digits++;
+    }
+    return digits;
+}
+
 
     function setMintPrice(uint256 tier, uint256 price) public onlyOwner {
         require(tier >= 1 && tier <= 4, "Invalid tier");
@@ -61,22 +71,18 @@ contract MyERC1155 is ERC1155, Ownable, ReentrancyGuard {
         return raisedCap;
     }
 
-    function checkIds(address _account, uint16 _tier) public view returns(uint256[] memory){
-        uint256[] memory ids = _nftIds[_account][_tier];
-        return ids;
+    // function checkIds(address _account, uint16 _tier) public view returns(uint256[] memory){
+    //     uint256[] memory ids = _nftIds[_account][_tier];
+    //     return ids;
+    // }
+
+    function setTokenURI(uint256 _tokenId, string memory _tokenURI) external onlyOwner {
+                _setURI( _tokenId, _tokenURI);
     }
 
-    function setTokenURI(uint256 tokenId, string memory _uri) public onlyOwner {
-        tokenURIs[tokenId] = _uri;
-    }
-
-    function uri(uint256 tierId) public view override returns (string memory) {
-        string memory tokenURI = tokenURIs[tierId];
-        return bytes(tokenURI).length > 0 ? tokenURI : super.uri(tierId);
-    }
-
-    function tokenUri(uint256 tierId, uint256 tokenId)public view returns (string memory) {
-        string memory tokenURI = tokenURIs[tierId];
-        return bytes(tokenURI).length > 0 ? string(abi.encodePacked(tokenURI, tokenId.toString())) : super.uri(tierId);
+    //optional
+    //if owner sets tokenURI then this if concat with that uri if set
+    function setBaseURI(string memory _baseURI) external onlyOwner{
+        _setBaseURI(_baseURI);
     }
 }
