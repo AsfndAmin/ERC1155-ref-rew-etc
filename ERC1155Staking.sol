85af954da5adab6e3a81edb8c833e512c106b302
Asfand Amin <99183246+AsfndAmin@users.noreply.github.com>
2023-05-13 20:22:57 +0500

    Create ERC1155Staking.sol

---
+++ ERC1155Staking.sol
@@ -0,0 +1,36 @@
+// SPDX-License-Identifier: MIT
+pragma solidity =0.8.17;
+
+import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/token/ERC1155/utils/ERC1155Holder.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Staking is ERC1155Holder, Ownable, ReentrancyGuard {
+
+    using SafeERC20 for IERC20;
+
+    struct NFTLock {
+        address owner;
+        uint256 nftId;
+        uint256 baseShares;
+        uint256 bonusShares;
+        uint256 timeLock;
+        uint256 unlockDate;
+    }
+
+    
+    // Mapping of staked NFT lock IDs to their lock details
+    mapping(uint256 => NFTLock) private NFTId;
+
+    // Mapping of staker addresses to their total shares
+    mapping(address => uint256) private stakerShares;
+
+    mapping(uint256 => uint256) public lockTimes;
+    mapping(uint256 => uint256) public baseShare;
+    mapping(uint256 => uint256) public bonusShare;
+    mapping(uint256 => uint256) public timeLockBonusShare;
+
+}
+
